712906803100ffb6baeb21330b38b408b4b6939e
Steven Gangl <steven.h.gangl@gmail.com>
2023-04-01 10:54:08 -0500

    added last page, it displays all the values...still need to make submut button do stuff

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios'
+
+
 
 function Review() {
-    console.log('review comp')
+    console.log('review comp', myFeeling)
+
+    const myFeeling = useSelector(store => store.feeling)
+    const myUnderstanding = useSelector(store => store.understanding)
+    const mySupport = useSelector(store => store.supported)
+    const myComments = useSelector(store => store.comments)
+
+
+
+
 
     return (
         <>
         <div>
             <h2>Review Your Feedback</h2>
+            <p>Feelings: {myFeeling}</p>
+            <p>Understanding: {myUnderstanding}</p>
+            <p>Support: {mySupport}</p>
+            <p>Comments: {myComments}</p>
+        </div>
+
+        <div>
+            <button>Submit</button>
         </div>
         </>
     )
